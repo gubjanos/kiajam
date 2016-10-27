f00cf869082e490137ef568c0d7d7b8a40540376
Janos Gub <jgub@hortonworks.com>
2016-10-27 23:23:49 +0200

    added isinsidecircle method

--- NokiaTowerJava/src/MapUtils.java
+++ NokiaTowerJava/src/MapUtils.java
@@ -43,6 +43,11 @@ public class MapUtils {
     return result;
   }
 
+  // Returns if a point (x1, y1) inside a (x2, y2) centered d radius circle
+  public static boolean isInsideCircle(int x1, int y1, int x2, int y2, int radius) {
+    return calculateSquaredDistance(x1, y1, x2, y2) <= (radius * radius);
+  }
+
   // Returns the squared distance of two points (x1,y1) and (x2,y2)
   public static int calculateSquaredDistance(int x1, int y1, int x2, int y2) {
     return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
