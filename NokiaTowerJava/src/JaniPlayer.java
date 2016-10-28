7392f3d91bcdf902414c9eea54f49304ec6537ec
Janos Gub <jgub@hortonworks.com>
2016-10-28 16:46:09 +0200

    saving game initialization state

--- NokiaTowerJava/src/JaniPlayer.java
+++ NokiaTowerJava/src/JaniPlayer.java
@@ -1,8 +1,11 @@
 public class JaniPlayer extends Player {
   private static Map map;
+  private static TheaderIni defaults;
   private static int[][][] populations; // time, x, y
+  private static int[][][] totalPopulations; // time, tower, radius
 
   // otletek: tornyonkent kiszamolni range-ekre az osszlakossagot koronkent: 200 * 365 * RANGE => 0.07MB * range
+  private static final int MAX_RADIUS = 100;
 
   public static void makeMove(TPlayer player) {
     if (player.myTime == 0) {
@@ -28,12 +31,25 @@ public class JaniPlayer extends Player {
   }
 
   private static void init(TPlayer player) {
+    // initialize characteristics
+    defaults = player.headerIni;
+
     map = player.map;
     populations = new int[Decl.TIME_MAX][][];
     populations[0] = map.pop;
+
     // calculating populations in advance
     for (int i = 1; i < Decl.TIME_MAX; i++) {
       populations[i] = MapUtils.popNextTime(populations[i-1], map);
+    }
+
+    // calculating total populations
+    totalPopulations = new int[Decl.TIME_MAX][][];
+    for (int i = 0; i < Decl.TIME_MAX; i++) {
+      // radius
+      for (int j = 0; j < MAX_RADIUS; j++) {
+
+      }
     }
   }
 }
