074076f50315cf7eb8f99653f4c9bdd4c7009108
Janos Gub <jgub@hortonworks.com>
2016-10-28 18:14:26 +0200

    workwork

--- NokiaTowerJava/src/JaniPlayer.java
+++ NokiaTowerJava/src/JaniPlayer.java
@@ -6,6 +6,10 @@ public class JaniPlayer extends Player {
   private static int[][][] populations; // time, x, y
   private static int[][][] towerPopulations; // time, tower, radius initialized by method calculateTowerStatistics
 
+  private static double[] dataNeedInTime; // time, the data need factor in time
+
+  private static int effectiveMaxRadius;
+
   private static Set<Integer> myTowers; // the set of towers owned
   private static Set<Integer> towersUnderOffer; // the towers we are offering to
   private static java.util.Map<Integer, TtowerOrderRec> towerOffers; // the offers given for towers
@@ -27,7 +31,13 @@ public class JaniPlayer extends Player {
       populations[i] = MapUtils.popNextTime(populations[i-1], map);
     }
 
-    calculateTowerStatistics(player);
+    // calculating data need increasement
+    dataNeedInTime[0] = 1.0d;
+    for (int i = 1; i < Decl.TIME_MAX; i++) {
+      dataNeedInTime[i] = dataNeedInTime[i-1] * state.dataMulti;
+    }
+
+    calculateTowerStatistics();
 
     myTowers = new HashSet<>();
     towersUnderOffer = new HashSet<>();
@@ -49,10 +59,11 @@ public class JaniPlayer extends Player {
     stepInGame(player);
   }
 
-  private static void calculateTowerStatistics(TPlayer player) {
+  // NOTE overlapping towers not taken into consideration
+  private static void calculateTowerStatistics() {
     // calculating total populations
     towerPopulations = new int[Decl.TIME_MAX][][];
-    int effectiveMaxRadius = Math.min(state.distMax - state.distMin, MAX_RADIUS_RANGE); // radius counted from distmin
+    effectiveMaxRadius = Math.min(state.distMax - state.distMin, MAX_RADIUS_RANGE); // radius counted from distmin
     int squaredMaximumDistance = effectiveMaxRadius * effectiveMaxRadius + state.distMin * state.distMin; // squared maximum distance from a tower
 
     for (int time = 0; time < Decl.TIME_MAX; time++) {
@@ -76,6 +87,31 @@ public class JaniPlayer extends Player {
       }
     }
 
+  }
+
+  // calculate
+  // maximum distance for a tower wrt data limit
+  // if a tower could not do any production distMin-1 is returned
+  private static int maximumDistance(short towerID, double dataTech, int time) {
+    for (int i = 0; i < effectiveMaxRadius; i++) {
+      double dataNeed = towerPopulations[time][towerID][i] * dataNeedInTime[time];
+      if (dataNeed > dataTech) return state.distMin + i - 1;
+    }
+
+    return effectiveMaxRadius + state.distMin;
+  }
+
+  // maximum revenue
+  // if there is no way to generate revenue at a tower with current technology returns -1
+  private static double maximumRevenue(short towerID, double dataTech, int time) {
+    int maximumDistance = maximumDistance(towerID, dataTech, time);
+    if (maximumDistance < state.distMin) return -1;
+    return towerPopulations[time][towerID][maximumDistance] * state.offerMax;
+  }
+
+  // cost of tower
+  private static double costOfTower(short towerID, TPlayer player) {
+    return player.inputData.towerInf[towerID].runningCost + player.inputData.towerInf[towerID].rentingCost;
   }
 
   private static void makeOffer(TPlayer player, short towerID, int offer, int distance) {
